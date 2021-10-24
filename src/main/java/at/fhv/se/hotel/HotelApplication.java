79f5abad95baa5184651c546bf6b697b4dacb2f6
SmightyM8 <michaelspiegel@live.at>
2021-10-24 16:35:16 +0200

    Initial project setup

---
+++ src/main/java/at/fhv/se/hotel/HotelApplication.java
@@ -0,0 +1,13 @@
+package at.fhv.se.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelApplication.class, args);
+    }
+
+}
