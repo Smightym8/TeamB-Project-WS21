6769df3ebe62d9c4fded5e787bd54ef8d4ee477d
Dario Birbamer0000 <dbi6423@students.fhv.at>
2021-11-23 18:39:06 +0100

    Created StayDTO and the respective Test for it

--- src/test/java/at/fhv/se/hotel/unit/application/StayDTOTest.java
+++ src/test/java/at/fhv/se/hotel/unit/application/StayDTOTest.java
@@ -1,2 +1,36 @@
-package at.fhv.se.hotel.unit.application;public class StayDTOTest {
+package at.fhv.se.hotel.unit.application;
+
+import at.fhv.se.hotel.application.dto.StayDTO;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StayDTOTest {
+
+    @Test
+    void given_staytodetails_when_createstayto_then_detailsequals() {
+        //given
+        String idExpected = "10";
+        String guestFirstNameExpected = "John";
+        String guestLastNameExpected = "Doe";
+        LocalDate checkInDateExpected = LocalDate.of(2021,11,23);
+        LocalDate checkOutDateExpected = LocalDate.of(2021, 12, 1);
+
+        //when
+        StayDTO stayDTOExpected = StayDTO.builder()
+                .withId(idExpected)
+                .withGuestFirstName(guestFirstNameExpected)
+                .withGuestLastName(guestLastNameExpected)
+                .withCheckInDate(checkInDateExpected)
+                .withCheckOutDate(checkOutDateExpected)
+                .build();
+
+        //then
+        assertEquals(idExpected,stayDTOExpected.id());
+        assertEquals(guestFirstNameExpected, stayDTOExpected.guestFirstName());
+        assertEquals(guestLastNameExpected, stayDTOExpected.guestLastName());
+        assertEquals(checkInDateExpected, stayDTOExpected.checkInDate());
+        assertEquals(checkOutDateExpected, stayDTOExpected.checkOutDate());
+    }
 }
