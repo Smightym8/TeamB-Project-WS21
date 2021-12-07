768c68599971b6dbc0c6076d859f843cc1ebb8fd
Ali Cinar <ali.cinar@outlook.at>
2021-12-07 11:36:22 +0100

    Tested class BookingWithRoomCategory.

--- src/test/java/at/fhv/se/hotel/unit/domain/BookingTests.java
+++ src/test/java/at/fhv/se/hotel/unit/domain/BookingTests.java
@@ -226,4 +226,19 @@ public class BookingTests {
         assertNotEquals(booking1, booking3);
         assertEquals(booking2, booking3);
     }
+
+    @Test
+    void given_3bookingids_2equals_1not_when_compare_then_2equals_1not() {
+        // given
+        String bookingIdStr1 = "1";
+        String bookingIdStr2 = "2";
+        BookingId bookingId1_1 = new BookingId(bookingIdStr1);
+        BookingId bookingId1_2 = new BookingId(bookingIdStr1);
+        BookingId bookingId2 = new BookingId(bookingIdStr2);
+
+        // when .. then
+        assertEquals(bookingId1_1, bookingId1_2);
+        assertNotEquals(bookingId1_1, bookingId2);
+        assertNotEquals(bookingId1_2, bookingId2);
+    }
 }
