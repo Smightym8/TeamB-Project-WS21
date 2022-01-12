f2c5af76bba8d7f5b0406894dc4616c3d4c9c73f
Michael Spiegel <michaelspiegel@live.at>
2022-01-12 15:01:09 +0100

    Added Routing and Bootstrap to SPA

---
+++ spa/src/components/HomeComponent.tsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class HomeComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="display-4">Hello, world!</h1>
+            </div>
+        );
+    }
+}
+
+export default HomeComponent;
